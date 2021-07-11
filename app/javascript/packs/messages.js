3338cc06ed27b98aa72c31b7d8d75d7fcb26f091
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-10 21:33:34 -0500

    eee

--- app/javascript/packs/messages.js
+++ app/javascript/packs/messages.js
@@ -2,9 +2,27 @@ import 'channels'
 import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import MessagesChannel from 'channels/messages_channel'
+import { csrfToken } from "@rails/ujs"
 
 const MessagesBoard = () => {
+  const [messages, setMessages] = useState([])
+  const [message, setMessagge] = useState('')
 
+  useEffect(() => {
+    MessagesChannel.recieved = (data) => setMessages(data.messages)
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await fetch('http://localhost:3000/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': csrfToken
+      },
+      body: JSON.stringify({ message })
+    })
+  }
 }
 
   document.addEventListener('DOMContentLoaded', () => {
