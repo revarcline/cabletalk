617a45c19ffd1ee42496c84cb0181f82f928783b
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-12 13:18:40 -0500

    barebones session logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user&.authenticate(params[:session][:password])
+      log_in_user
+    else
+      # send error
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
